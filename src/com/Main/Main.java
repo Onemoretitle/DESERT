7675e81aa90b176c5cd4e10e4f6c3bf5e3eeb3ef
Kamo98 <avanesjan.k@gmail.com>
2018-07-01 19:07:04 +0700

    Update Main.java

--- src/com/Main/Main.java
+++ src/com/Main/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Это сделал не я");
         try {
             Starter startingPoint = new Starter();
         } catch (Exception e) {
